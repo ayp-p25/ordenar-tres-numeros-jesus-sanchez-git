d5d51bbc5722cac77c85cc6ad93051266d70b5d9
jesusa-sanchez-git <jesusa.sanchez@iteso.mx_PianoMan11VJ>
2025-02-19 23:42:00 -0600

    First

--- ordenar3.py
+++ ordenar3.py
@@ -1,15 +1,37 @@
+#Datos
 """
-Inserta el encabezado aquí y escribe tu código abajo
+No 6 : Dados tres números, ordenarlos de mayor a menor. 
+Jesús Alejandro Sánchez Muñoz
+758377 
+Igenieria Mecanica 
+Algoritmos y Programcion 
+ESI232B2 
+Jorge Zaldivar
+18/Feb/25 
+10 minutos de duracion
 """
 
-# Declaraciones
-CONSTANTE = valor
 
 # Entradas
-entrada = input()
+A = float(input("Ingrese su primer numero :  \n"))
+B = float(input("Ingrese su segundo numero : \n"))
+C = float(input("Ingrese un tercer numero : \n"))
+
+if A > B > C :
+    print("El orden de mayor a menor es : " ,A , B, C)
+elif B > C >A:
+    print("El orden de mayor a menor es : " ,B ,C ,A )
+elif C > A > B:
+    print("El orden de mayor a menor es : " ,C ,A ,B )
+elif A > C > B:
+    print("El orden de mayor a menor es : ", A, C, B)
+elif C > B > A:
+    print("El orden de mayor a menor es : " ,C ,B ,A )
+elif B > A > C:
+    print("El orden de mayor a menor es : " ,B ,A ,C )
 
 # Proceso
 
 
 # Salidas
-print(salida)
+
